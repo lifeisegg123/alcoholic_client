ddf80d6ce71d1314591863ff2686cb0c277d5445
lifeisegg123 <leejj2002@naver.com>
2021-03-21 15:17:54 +0900

    add favicon, prepare for seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,9 +16,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link
+            rel="shortcut icon"
+            href="https://drinklikeafish.xyz/favicon.ico"
+          />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content="주당 이선생" />
           <meta property="og:title" content="주당 이선생" />
+          <meta name="description" content="술 정보 공유 및 리뷰 사이트" />
           <meta
             property="og:description"
             content="술 정보 공유 및 리뷰 사이트"
